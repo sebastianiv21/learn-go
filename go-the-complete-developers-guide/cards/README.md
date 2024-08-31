d948ed4023e01fafe3813e9801ac289bb8f8529e
sebastianiv21 <seluti98@gmail.com>
2024-08-30 22:05:54 -0500

    session 1

---
+++ go-the-complete-developers-guide/cards/README.md
@@ -0,0 +1,7 @@
+# CARDS PROJECT
+
+![[./assets/cards_project.jpeg]]
+
+## Go approach to OOP
+
+![[./assets/go_approach_to_oop.jpeg]]
---
+++ go-the-complete-developers-guide/cards/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades"
+	cards := newDeck()
+
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+}
