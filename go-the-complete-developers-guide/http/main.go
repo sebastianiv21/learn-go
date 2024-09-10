b3fb6d4c8d621ca6d28bfb191fe192f79b9857ba
sebastianiv21 <seluti98@gmail.com>
2024-09-09 21:10:01 -0500

    session 6

---
+++ go-the-complete-developers-guide/http/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	io.Copy(os.Stdout, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+
+	return len(bs), nil
+}
